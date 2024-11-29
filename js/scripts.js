827c9e53488d3df7e8c695559bcbf05bb36ab09f
Dimanoti <141250857+Dimanoti@users.noreply.github.com>
2024-11-29 13:11:26 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
-// Get the modal
-var signInModal = document.getElementById("signInModal");
-var signUpModal = document.getElementById("signUpModal");
+document.addEventListener('DOMContentLoaded', function() {
+  var signInDialog = document.getElementById('signin-dialog');
+  var signUpDialog = document.getElementById('signup-dialog');
 
-// Get the button that opens the modal
+  document.querySelector('.js-open-signin').addEventListener('click', function() {
+    signInDialog.style.display = 'flex';
+    signUpDialog.style.display = 'none';
+  });
 
-// Function to open the Sign In Modal
-function openModal(modalId) {
-  if (modalId === 'signIn') {
-    signInModal.style.display = "block";
-  } else if (modalId === 'signUp') {
-    signUpModal.style.display = "block";
-  }
-}
+  document.querySelector('.js-open-signup').addEventListener('click', function() {
+    signUpDialog.style.display = 'flex';
+    signInDialog.style.display = 'none';
+  });
 
-// Function to close the modal
-function closeModal(modalId) {
-  if (modalId === 'signInModal') {
-    signInModal.style.display = "none";
-  } else if (modalId === 'signUpModal') {
-    signUpModal.style.display = "none";
-  }
-}
+  document.querySelectorAll('.js-close-dialog').forEach(function(closeButton) {
+    closeButton.addEventListener('click', function() {
+      signInDialog.style.display = 'none';
+      signUpDialog.style.display = 'none';
+    });
+  });
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == signInModal || event.target == signUpModal) {
-    signInModal.style.display = "none";
-    signUpModal.style.display = "none";
-  }
-}
+  document.querySelector('.js-open-signup-dialog').addEventListener('click', function() {
+    signUpDialog.style.display = 'flex';
+    signInDialog.style.display = 'none';
+  });
+
+  document.querySelector('.js-open-signin-dialog').addEventListener('click', function() {
+    signInDialog.style.display = 'flex';
+    signUpDialog.style.display = 'none';
+  });
+});
